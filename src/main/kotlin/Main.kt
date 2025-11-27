a003d52853530d2cba072fd7e040faa70c96353d
Rio Wickham <wickham.rio@gmail.com>
2025-11-27 18:35:10 +0000

    issue 1 updated code for use pre testing, big upgrade on our user menu and do while loops, new functions created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,7 @@ import models.Employee
 import models.Payslip
 
 private val payslipController = PayslipController()
+private val appController = AppController()
 
 private var employee: Employee? = null
 private var payslip: Payslip? = null
@@ -36,12 +37,12 @@ fun mainMenu(): Int {
         > | Info Menu               |
         > |   1) Log Hourly Pay     |
         > |   2) Log Sunday Pay     |
-        > |   3) Commission Tool    |
-        > |   4) Log Hours for slip |
-        > |   5) Payment Frequency  |
-        > |   6) Employee ID        |
+        > |   4) Payment Frequency  |
+        > |   5) Employee ID        |
         > ---------------------------
-        > |   7) Print Slip         |
+        > | Payslip Menu            |
+        > |   6) Create New Payslip |
+        > |   7) View All Payslips  |
         > ---------------------------
         > |   0) Exit               |
         > ---------------------------
@@ -56,11 +57,10 @@ fun runMenu() {
         when (option) {
             1 -> logHourlyPay()
             2 -> logSundayPay()
-            3 -> commissionsTool()
-            4 -> logHours()
-            5 -> paymentFrequency()
-            6 -> employeeID()
-            7 -> printPayslip()
+            4 -> paymentFrequency()
+            5 -> employeeID()
+            6 -> createNewPayslip()
+            7 -> viewAllPayslips()
             0 -> exit()
             else -> println("!Invalid Option!")
         }
@@ -69,10 +69,12 @@ fun runMenu() {
 
 fun logHourlyPay(){
     employee?.hourlyPay = readNextDouble("What is your hourly pay?: ")
+    println("Hourly pay updated successfully!")
 }
 
 fun logSundayPay(){
     employee?.sundayPay = readNextDouble("What is your pay for Sundays?: ")
+    println("Sunday pay updated successfully!")
 }
 
 fun commissionsTool(){
@@ -85,12 +87,13 @@ fun logHours(){
 }
 
 fun paymentFrequency() {
-    employee?.payFreq = readNextInt("Do you get paid (1) Weekly, (2) Biweekly, (3) Monthly:")
+    employee?.payFreq = readNextInt("Do you get paid (1) Weekly, (2) Biweekly, (3) Monthly: ")
+    println("Payment frequency updated successfully!")
 }
 
 fun employeeID() {
     employee?.employeeId = readNextInt("Please Enter Your Employee ID: ")
-    payslip?.employeeId = readNextInt("Confirm Employee ID Again: ")
+    println("Employee ID updated successfully!")
 }
 
 fun printPayslip(){
@@ -102,7 +105,64 @@ fun printPayslip(){
     println(payslipController.printPayslip(employee!!, payslip!!))
 }
 
+
+
+fun createNewPayslip() {
+    if (employee == null) {
+        println("Error: No employee information available!")
+        return
+    }
+
+    if (employee!!.hourlyPay == 0.0) {
+        println("Please set your hourly pay first (Option 1)!")
+        return
+    }
+
+    // Get/confirm employee ID for this payslip
+    val employeeId = readNextInt("Please enter your Employee ID: ")
+    employee!!.employeeId = employeeId
+
+    // Create new payslip
+    val newPayslip = Payslip(
+        employeeId = employeeId,
+        employee = employee!!
+    )
+
+    // Get payslip details
+    newPayslip.normalHours = readNextDouble("Please enter your hours worked for this period (excluding breaks): ")
+    newPayslip.sundayHours = readNextDouble("Please enter your hours worked for Sunday(s): ")
+    newPayslip.comForPeriod = readNextDouble("Please enter commissions gained for this pay period (0 for none): ")
+
+    // Save the payslip
+    appController.addPayslip(newPayslip)
+
+    println("\n=== Payslip Created Successfully! ===")
+    println(payslipController.printPayslip(employee!!, newPayslip))
+}
+
+fun viewAllPayslips() {
+    val employeeId = readNextInt("Please enter your Employee ID to view payslips: ")
+
+    val payslips = appController.getPayslipsByEmployeeId(employeeId)
+
+    if (payslips.isEmpty()) {
+        println("No payslips found for Employee ID: $employeeId")
+        return
+    }
+
+    // Get employee info from the payslips (they all belong to the same employee ID)
+    val employeeInfo = payslips.first().employee
+
+    println("\n=== All Payslips for ${employeeInfo.name} (ID: $employeeId) ===")
+    payslips.forEachIndexed { index, payslip ->
+        println("\n--- Payslip #${index + 1} ---")
+        println(payslipController.printPayslip(payslip.employee, payslip))
+    }
+
+    println("\nTotal payslips: ${payslips.size}")
+}
+
 fun exit() {
-    println("Exiting!")
-    return
+    println("Exiting! Thank you for using Payslip Pal.")
+    kotlin.system.exitProcess(0)
 }
